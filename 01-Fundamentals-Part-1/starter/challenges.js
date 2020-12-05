c1e2e0796a47081894ea9fff4736d8b4093a49d4
Catrina Fuentes <cat.fuentes93@gmail.com>
2020-12-04 16:53:55 -0800

    solved all coding challenges for part 1

--- 01-Fundamentals-Part-1/starter/challenges.js
+++ 01-Fundamentals-Part-1/starter/challenges.js
@@ -19,5 +19,37 @@ else {
     console.log (`Mark's BMI (${markBMI} is higher than John's ${johnBMI})!`);
 }
 
+// challenge #3, using bonus data set #1
+const dolphinScore = [97, 112, 101];
+const koalaScore = [109, 95, 123];
+let dolphSum = 0;
+let kolSum = 0;
 
+for (let i = 0; i < dolphinScore.length; i++) {
+    dolphSum += dolphinScore[i];
+}
 
+for (let i = 0; i < koalaScore.length; i++) {
+    kolSum += koalaScore[i];
+}
+
+const dolphAvg = Math.floor(dolphSum / dolphinScore.length); 
+const kolAvg = Math.floor(kolSum / koalaScore.length); 
+
+if (dolphAvg > kolAvg && dolphAvg >= 100) {
+    console.log("Dolphins win!");
+}
+else if (kolAvg > dolphAvg && kolAvg >= 100) {
+    console.log("Koalas win!");
+}
+else if (kolAvg === dolphAvg && kolAvg >= 100 && dolphAvg >= 100) {
+    console.log("It's a tie!");
+}
+else {
+    console.log("No one wins :(");
+}
+
+// challenge #4
+const bill = 275; 
+const tip = bill <= 300 && bill >= 50 ? bill * .15 : bill * .20; 
+console.log(`The bill was ${bill}, the tip was ${tip}. Overall it cost ${bill + tip}.`);
