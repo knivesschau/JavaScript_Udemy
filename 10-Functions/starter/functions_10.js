196bddb471ce93fcc302010337cbd2c3eec24876
Catrina Fuentes <cat.fuentes93@gmail.com>
2021-01-08 20:00:04 -0800

    completed reference exercises

--- 10-Functions/starter/functions_10.js
+++ 10-Functions/starter/functions_10.js
@@ -22,4 +22,37 @@ bookFlight('PA124');
 bookFlight('PA124', 5, 350);
 bookFlight('PA124', 7);
 bookFlight('PA124', 2);
-bookFlight('PA124', undefined, 8); // bypassing a default parameter and only changing one parameter
+bookFlight('PA124', undefined, 8); // bypassing a parameter and only changing one parameter
+
+// argument passing exercises
+const flight = "LH348";
+const passengerInfo = {
+    name: 'Princess Peach',
+    passport: 2304910293
+};
+
+const checkIn = function(flightNum, passenger) {
+    flightNum = "BE130";
+    passenger.name = 'Mrs. ' + passenger.name;
+    
+    if (passenger.passport === 2304910293) {
+        console.log('Checked in!');
+    }
+    else {
+        console.log('Wrong passport info!');
+    }
+};
+
+// checkIn(flight, passengerInfo);
+console.log(flight); // does not change! primitive type values are copied, but it is not MUTATED
+console.log(passengerInfo); // copying the REFERENCE to the object in memory, thats why it mutates
+
+const newPassport = function(person) {
+    person.passport = Math.trunc(Math.random() * 10000000000);
+}
+
+newPassport(passengerInfo);
+checkIn(flight, passengerInfo); // passport info no longer the same, so it alters the correct/incorrect passport prompts 
+
+// javascript passes BY VALUE, not by reference! 
+
