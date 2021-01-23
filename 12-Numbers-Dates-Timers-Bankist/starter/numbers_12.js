e5cd78e8007ab634487aecff9f3abb0304d5ce5b
Catrina Fuentes <cat.fuentes93@gmail.com>
2021-01-22 17:26:48 -0800

    completed converting and number checking exercises

--- 12-Numbers-Dates-Timers-Bankist/starter/numbers_12.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/numbers_12.js
@@ -164,7 +164,7 @@ btnLogin.addEventListener('click', function (e) {
   );
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
@@ -182,7 +182,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = +inputLoanAmount.value;
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -223,7 +223,7 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
@@ -251,3 +251,23 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+// converting and checking numbers exercises
+console.log(+'23'); // type coercion into number
+
+console.log(Number.parseInt('30px', 10)); // parsing, string MUST start with a number
+console.log(Number.parseFloat('2.5rem')); // parse floating point
+
+console.log(Number.isNaN(20)); // is NaN (check if its literally NaN)
+console.log(Number.isNaN('20'));
+console.log(Number.isNaN(+'20X'));
+console.log(Number.isNaN(23 / 0));
+
+console.log(Number.isFinite(20)); // is finite (use to check if value is a number)
+console.log(Number.isFinite('20'));
+console.log(Number.isFinite(+'20X'));
+console.log(Number.isFinite(23 / 0));
+
+console.log(Number.isInteger(23)); // is integer (use to check if value is an integer)
+console.log(Number.isInteger(23.0));
+console.log(Number.isInteger(23 / 0));
