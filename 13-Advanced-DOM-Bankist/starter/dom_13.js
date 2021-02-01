e115949984e9e62125789e43a6429c7303792916
Catrina Fuentes <cat.fuentes93@gmail.com>
2021-02-01 14:34:04 -0800

    added smooth scrolling to bankist app, finished first few lectures

--- 13-Advanced-DOM-Bankist/starter/dom_13.js
+++ 13-Advanced-DOM-Bankist/starter/dom_13.js
@@ -30,41 +30,89 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-// Lecture on Element Selection //
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
+// Smooth scrolling for Bankist App //
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
-const header = document.querySelector('.header');
+btnScrollTo.addEventListener('click', function (e) {
+  // const s1coords = section1.getBoundingClientRect();
 
-const allSections = document.querySelectorAll('.section'); // multiple element selector
-console.log(allSections);
+  // universally browser-compatible method
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
 
-document.getElementById('section--1'); // do not need selector for getElementById
+  // modern method (does not work with old browsers)
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
 
-const allButtons = document.getElementsByTagName('button');
-console.log(allButtons);
+// Lectures on Element Selection + Styles, Attributes, + Classes //
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
 
-document.getElementsByClassName('btn'); // do not need selector either
+// const header = document.querySelector('.header');
+
+// const allSections = document.querySelectorAll('.section'); // multiple element selector
+// console.log(allSections);
+
+// document.getElementById('section--1'); // do not need selector for getElementById
+
+// const allButtons = document.getElementsByTagName('button');
+// console.log(allButtons);
+
+// document.getElementsByClassName('btn'); // do not need selector either
 
 // Creating and inserting elements
-const message = document.createElement('div');
-message.classList.add('cookie-message');
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
 // message.textContent =
 //   'We use cookies for improved functionality and analytics.';
-message.innerHTML =
-  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+// message.innerHTML =
+//   'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
 
 // header.prepend(message); // make element first child
-header.append(message); // make element last child
+// header.append(message); // make element last child
 // header.append(message.cloneNode(true));
 // header.before(message);
 // header.after(message);
 
 // Delete elements
-document
-  .querySelector('.btn--close-cookie')
-  .addEventListener('click', function () {
-    message.remove(); //  modern method
-    // message.parentElement.removeChild(message); // old way of removal
-  });
+// document
+//   .querySelector('.btn--close-cookie')
+//   .addEventListener('click', function () {
+//     message.remove(); //  modern method
+// message.parentElement.removeChild(message); // old way of removal
+//   });
+
+// Styles
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
+// console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).height);
+
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+// Attributes
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.className);
+
+// logo.alt = 'Beautiful minimalist logo';
+// logo.setAttribute('company', 'Bankist');
+
+// const link = document.querySelector('.nav__link--btn');
+// console.log(link.href);
+// console.log(link.getAttribute('href'));
+
+// Data Attributes
+// console.log(logo.dataset.versionNumber);
+
+// Classes
+// logo.classList.add('c');
+// logo.classList.remove('c');
+// logo.classList.toggle('c');
+// logo.classList.contains('c');
