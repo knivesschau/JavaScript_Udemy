64623f2375fda37835c52a0cc5560693f038c6d7
Catrina Fuentes <cat.fuentes93@gmail.com>
2021-02-24 18:45:01 -0800

    added smooth scrolling for nav links

--- 13-Advanced-DOM-Bankist/starter/dom_13.js
+++ 13-Advanced-DOM-Bankist/starter/dom_13.js
@@ -48,6 +48,18 @@ btnScrollTo.addEventListener('click', function (e) {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
 
+// Page Navigation + Smooth Scrolling //
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+    });
+  }
+});
+
 // Event Propogation Exercises //
 // const randomInt = (min, max) =>
 //   Math.floor(Math.random() * (max - min + 1) + min);
